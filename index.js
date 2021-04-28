90e5fdb1b08ceb8d3e7a2a0dc4e69b30bc459e8c
Liz Burton <liz.burton147@gmail.com>
2021-04-28 11:19:17 -0400

    Adds solution

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function receivesAFunction(callback) {
+    callback();
+}
 
+function returnsANamedFunction() {
+    const namedFn = () => console.log("this is a named function");
+    return namedFn;
+}
+
+function returnsAnAnonymousFunction() {
+    return () => console.log("this is an anonymous function");
+}
