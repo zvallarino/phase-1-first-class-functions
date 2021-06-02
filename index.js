cf0b0987aa3a719584aa2ad237127873dbfcf30c
ihollander <ianholla@gmail.com>
2021-06-02 08:47:55 -0400

    Updated solution

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function receivesAFunction(callback) {
-    callback();
+  callback();
 }
 
 function returnsANamedFunction() {
-    const namedFn = () => console.log("this is a named function");
-    return namedFn;
+  return function namedFn() {
+    console.log("this is a named function");
+  };
 }
 
 function returnsAnAnonymousFunction() {
-    return () => console.log("this is an anonymous function");
+  return () => console.log("this is an anonymous function");
 }
